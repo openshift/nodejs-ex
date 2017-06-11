c829cf58ef97633d5a6f3b293bf9716c55cae0db
joewaa <joewaa@users.noreply.github.com>
2017-06-11 08:52:50 +0800

    require todo model

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(app) {
-  //var todoList = require('../controllers/todoListController');
+  var todoList = require('../controllers/todoListController');
   
   app.route('/api/test')
     .get((req,res)=>{ res.send('testing here'); });
