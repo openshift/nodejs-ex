7335ec926a94c44f6f6a2bed11f276cf9f5ab6e2
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-11-03 22:56:07 -0400

    Fix Port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var  morgan  = require('morgan');
 
 //OpenShift Settings
 var ip = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 'localhost';
-var port = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 3000;
+var port = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 8080;
 
 //Reporting
 webapp.use(morgan('combined'));
