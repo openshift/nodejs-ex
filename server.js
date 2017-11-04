a3145f6840b569363fa31f8375c2414900b1a495
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-11-04 10:24:28 -0400

    Check Route

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var	webapp = express();
 var	Promise = require('promise');
 var  morgan  = require('morgan');
 
-console.log('process.env = ' + JSON.stringify(process.env));
-
 //OpenShift Settings
 var ip = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 'localhost';
 var port = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 8080;
@@ -16,7 +14,7 @@ webapp.use(morgan('combined'));
 //External Routing
 
 webapp.get('/', function(req, res) {
-    console.log(__dirname);
+    console.log('route correct');
     //res.sendFile(__dirname + '/public/views/pages/index.html');
     res.send('works');
 });
