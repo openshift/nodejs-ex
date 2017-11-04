68d1ba759177fd3d66d7478963d397214c60bece
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-11-04 19:22:58 -0400

    Webapp to app

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Dependencies
 var express = require('express');
-var	webapp = express();
+var	app = express();
 //var	Promise = require('promise');
 //var  morgan  = require('morgan');
 
@@ -9,19 +9,19 @@ var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 //Reporting
-//webapp.use(morgan('combined'));
+//app.use(morgan('combined'));
 
 //External Routing
-webapp.get('/', function (req, res) {
+app.get('/', function (req, res) {
   console.log('works as expected');
   res.send('<p>some html</p>');
   //res.send('Hello World Again!');
 });
 
-//webapp.use('/public', express.static(__dirname + '/public'));
+//app.use('/public', express.static(__dirname + '/public'));
 
-//Webapp Initialize
-webapp.listen(port, ip);
+//app Initialize
+app.listen(port, ip);
 //console.log('live on ip: ' + ip + ':' + port);
 console.log('Server running on http://%s:%s', ip, port);
 //console.log(JSON.stringify(process.env));
