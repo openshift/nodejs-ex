826555024ec0e952f539cfc0386cbadf96d7562a
briandavidjoyner <brianjoyner@Brians-MBP.home>
2017-11-05 00:29:53 -0400

    Speedup

--- server.js
+++ server.js
@@ -2,11 +2,18 @@
 var express = require('express');
 var	app = express();
 var	Promise = require('promise');
+var compression = require('compression');
+
 var API = require(__dirname + '/API.js');
 
 //OpenShift Settings
-var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//var ip = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+//var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = 8080;
+var ip = 'localhost'
+
+//Middleware
+app.use(compression());
 
 //External Routing Of Public Assets
 app.use('/public', express.static(__dirname + '/public'));
