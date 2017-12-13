915d00f9ced6f536377fb9f6b8292ae01f8839d2
krzysiekbr <kbr@tutanota.com>
2017-12-13 21:07:32 +0000

    add new endpoint

--- server.js
+++ server.js
@@ -92,6 +92,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/timeline', function (req, res) {
+  res.render('timeline.html', { events: event });
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
