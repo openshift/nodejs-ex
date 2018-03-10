19afff7da983e9b48cdc03f7400f80d9812eba8e
Aivann Cariaga <van_cariaga@yahoo.com>
2018-03-11 01:52:46 +0800

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ wss.on('connection', function connection(ws, req) {
   ws.send('something');
 });
 
-server.listen(8080, function listening() {
+server.listen(8081, function listening() {
   console.log('Listening on %d', server.address().port);
 });
 
