8ea087c6a5a5633a42e2efc15babac71221831b3
Aivann Cariaga <van_cariaga@yahoo.com>
2018-03-17 05:17:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,9 @@ mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 
 //Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on('error', function(){
+    console.error.bind(console, 'MongoDB connection error:');
+});
 
 db.once('open', function() {
   // we're connected!
