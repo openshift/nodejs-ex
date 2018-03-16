48fc7616c6f4a6f633205cba18445a10e6d93e31
Aivann Cariaga <van_cariaga@yahoo.com>
2018-03-17 06:05:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(morgan('combined'))
 //you can get the user and password from Application>secrect
 //you can get the IP from the Cluster IP
 
-var mongoDB = 'mongodb://userODO:SLkhNIbks6EDAAeK@172.30.195.230:27017/sampledb';//creates if not exist
+var mongoDB = 'mongodb://userODO:SLkhNIbks6EDAAeK@172.30.77.50:27017/sampledb';//creates if not exist
 // if OPENSHIFT env variables are present, use the available connection info:
 /*if (process.env.OPENSHIFT_MONGODB_DB_URL) {
     url = process.env.OPENSHIFT_MONGODB_DB_URL +
