061b56b9abdfe614992896077275ab4624088267
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-03-24 22:23:19 +0800

    asf

--- models/genericTestModel.js
+++ models/genericTestModel.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var GlobalConstant = require('../GlobalConstant/mongoConnection');
-mongoose.connect(GlobalConstant.ConnectionString);
+mongoose.connect(exports.ConnectionString());
 
 let employeeSchema = new Schema({
     name: String,
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-console.log(mongoConnection.ConnectionString);
+console.log(exports.ConnectionString());
 
 /*
 console.log(process.env.mongoURL);
@@ -78,7 +78,7 @@ server.listen(8080, function listening() {
 //you can get the user and password from Application>secrect
 //you can get the IP from the Cluster IP
 
-var mongoDB = mongoConnection.ConnectionString;//creates if not exist
+var mongoDB = exports.ConnectionString();//creates if not exist
 console.log("Attempt to Connect to mongodb : "+mongoDB);
 mongoose.connect(mongoDB);
 console.log("Mongose Connection state : "+mongoose.connection.readyState);
