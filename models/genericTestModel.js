911242de907633810366459ad23135b08e9394c0
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-03-25 02:25:17 +0800

    453

--- models/genericTestModel.js
+++ models/genericTestModel.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 var GlobalConstant = require('../GlobalConstant/mongoConnection');
-mongoose.connect(GlobalConstant.conn);
+mongoose.connect(GlobalConstant.connectionStr);
 
 let employeeSchema = new Schema({
     name: String
