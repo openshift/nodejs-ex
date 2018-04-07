c002907836584be3ff9af9e864fc39b890b38891
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-07 12:09:27 +0800

    sdg

--- Controllers/SocketController.js
+++ Controllers/SocketController.js
@@ -1,4 +1,7 @@
-var express = require('express');
+var express = require('express'),
+    app     = express(),
+    morgan  = require('morgan');
+    
 var router = express.Router();
 const WebSocket = require('ws');
 app.engine('html', require('ejs').renderFile);
