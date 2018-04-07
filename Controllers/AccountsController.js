d12d0491e654ca458f2a97812d60704f0ad39b04
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-07 08:33:28 +0800

    asf

--- Controllers/AccountsController.js
+++ Controllers/AccountsController.js
@@ -26,10 +26,9 @@ router.get('/', function(req, res){
       })
       .then(function(doc){
         console.log("----------");
-        console.log(JSON.stringify({items:doc}));
-        res.send(JSON.stringify({items:doc}));
+        console.log(JSON.stringify({items:doc}, undefined, '\ '));
+        res.send(JSON.stringify({items:doc}, undefined, '\ '));
         console.log("----------");
-        console.log({items:doc});
       });
     
  });
