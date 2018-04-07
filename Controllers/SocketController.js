e41fafec9def5bde1c74d946e8a8d49698b287c2
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-07 14:05:42 +0800

    asf

--- Controllers/SocketController.js
+++ Controllers/SocketController.js
@@ -4,8 +4,7 @@ var express = require('express'),
 
 var router = express.Router();
 const WebSocket = require('ws');
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
+
 
 var server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
