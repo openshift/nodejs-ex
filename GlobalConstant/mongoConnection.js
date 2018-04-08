7d0c471dcd971f000428ea3f0d05a285cbc576d0
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-08 14:02:29 +0800

    asf

--- GlobalConstant/mongoConnection.js
+++ GlobalConstant/mongoConnection.js
@@ -1,3 +1,6 @@
 //let x = ;
-var conn = "mongodb://userQNU:ORwhNq7eYeyuyTx5@172.30.253.141:27017/sampledb";
+//you can retrive the ip from the cluster ip
+//upon creation you can use the userQNU as the user
+//while for the password you can use ORwhNq7eYeyuyTx5 also upon creation
+var conn = "mongodb://userQNU:ORwhNq7eYeyuyTx5@172.30.159.168:27017/sampledb";
 module.exports.connectionStr = conn;
