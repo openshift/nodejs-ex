907501f557da545a80a096a8bc64a135e93e70ea
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-08 15:59:43 +0800

    453

--- server.js
+++ server.js
@@ -63,11 +63,12 @@ console.log('test socket');
 var server = require('http').createServer(app);
 const wss = new WebSocket.Server({ server });
 wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    console.log("somthing from the client "+ data);
-  });
   ws.send('something');//send to the new connect
+  ws.on('message', function incoming(data) {
+    console.log('somthing from client'+ data);
+  });
 });
+
 
 /*
 wss.on('connection', function connection(ws, req) {
