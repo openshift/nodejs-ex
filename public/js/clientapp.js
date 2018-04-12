e0db0fd5d3753b7b8f8be8ec33edb7d387c1a932
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-12 14:16:50 +0800

    dfh
    dfh

--- public/js/clientapp.js
+++ public/js/clientapp.js
@@ -1,4 +1,6 @@
 //all client bundle includes
 //non server side
 //always added to the bottom of an html
+//global constant names and jsons
 console.log('clientappjs worked?');
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
