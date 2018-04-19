0a2abb931f3c2a9d13aa65f25b28b904d5d8b6e6
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-19 08:35:48 +0800

    asd
    as

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     //console.log('received: %s', message);
   });
- // ws.send('something');
+   ws.send('something');
 });
 
 
