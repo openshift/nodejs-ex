57da06a64559c9ef5c4fd20a8e37d3ee7751ada4
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-20 15:46:37 +0800

    root
    root

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ wss.on('connection', function connection(ws,req) {
 
 
   //send connection information after connecting 
-  ws.send(JSON.stringify(userobject), function ack(error) {//send the message with error check
+  ws.send(JSON.stringify(root), function ack(error) {//send the message with error check
     // If error is not defined, the send has been completed, otherwise the error
     // object will indicate what failed.
     console.log(error);
