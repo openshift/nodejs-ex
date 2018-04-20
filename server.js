2868340cb344df064cb753a45552578793a80a6d
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-20 12:29:39 +0800

    latestconnection added
    latestconnection added

--- server.js
+++ server.js
@@ -109,9 +109,10 @@ wss.on('connection', function connection(ws,req) {
     });
   });
   
+  let latestconnection ={ip:ws.ip,id:ws.id};
   
   //send connection information after connecting 
-  ws.send(JSON.stringify({ip:ws.ip,id:ws.id}), function ack(error) {//send the message with error check
+  ws.send(JSON.stringify(latestconnection), function ack(error) {//send the message with error check
     // If error is not defined, the send has been completed, otherwise the error
     // object will indicate what failed.
     console.log(error);
