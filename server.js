a76d3678a0831c9a161d1feba8fcb056d76323e5
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-22 06:22:37 +0800

    asf
    asf

--- server.js
+++ server.js
@@ -192,11 +192,11 @@ async.forever(
 
 
   //send connection information after connecting 
-  ws.send(JSON.stringify(root), function ack(error) {//send the message with error check
+  /*ws.send(JSON.stringify(root), function ack(error) {//send the message with error check
     // If error is not defined, the send has been completed, otherwise the error
     // object will indicate what failed.
     console.log(error);
-  });
+  });*/
 
 
 
