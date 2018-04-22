7433f523ffaae126e6546419b5da74ce8f565659
cariaga.info@gmail.com <cariaga.info@gmail.com>
2018-04-22 14:53:41 +0800

    asf
    asf

--- server.js
+++ server.js
@@ -97,12 +97,12 @@ wss.on('connection', function connection(ws,req) {
 
       this.root =[]; //root representing an array of json
       let userobject={};// user the information of other connections for connection
-      this.userobject.type='';//the identifer type of object
-      this.userobject.time='';
+      this.userobject.type="";//the identifer type of object
+      this.userobject.time="";
       this.userobject.usercontainer=[];
       let user ={};
-      this.user.ip='';
-      this.user.connectionid='';
+      this.user.ip="";
+      this.user.connectionid="";
       this.user.otherConnection=[];//object containing other
       this.userobject.usercontainer.push(user);
       this.root.push(userobject);
